95637381ad98299424578f0fe94d1f1422b3fe25
mZaiam <matheus.z.monteiro@gmail.com>
2025-06-19 20:40:53 -0300

    adding RANFIS to __init__

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="anfispy",
-    version="0.2.0",
+    version="0.2.1",
     author="Matheus Zaia Monteiro",
     author_email="matheus.z.monteiro@gmail.com",
     url="https://github.com/mZaiam/ANFISpy",
